78f5cd4e7cb8546ebffd8e11c352d8ec7138504b
Akshat Rastogi <rastogiakshat10@gmail.com>
2024-11-22 02:00:29 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common'; // Use CommonModule for Angular directives
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,        
+    MatToolbarModule,   
+    MatButtonModule,   
+    MatIconModule        
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Task_app';
